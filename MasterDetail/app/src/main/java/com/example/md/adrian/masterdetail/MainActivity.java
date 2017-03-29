d6fd502946e845dba0e22eda564c11882e6071c5
unknown <piotr.alejski@gmail.com>
2017-03-29 19:19:45 +0200

    add new button

--- MasterDetail/app/src/main/java/com/example/md/adrian/masterdetail/MainActivity.java
+++ MasterDetail/app/src/main/java/com/example/md/adrian/masterdetail/MainActivity.java
@@ -2,39 +2,40 @@ package com.example.md.adrian.masterdetail;
 
 import android.app.Activity;
 import android.app.Fragment;
-import android.app.FragmentManager;
 import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends Activity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
+//        showBasicData();
     }
 
     public void showBasicData() {
         DetailFragment detailFragment = getDetailFragment();
-        if (detailFragment != null && detailFragment.isInLayout()) {
+        if (detailFragment != null) {
             detailFragment.showBasicData();
-        }
-        else {
-            //Uruchamiał nową aktywność
+        } else {
             Intent intent = new Intent(this, DetailActivity.class);
+            intent.putExtra(DetailActivity.TO_SHOW, DetailActivity.BASIC_DATA);
             startActivity(intent);
         }
     }
 
     public void showDetailsData() {
         DetailFragment detailFragment = getDetailFragment();
-        detailFragment.showDetailsData();
+        if (detailFragment != null) {
+            detailFragment.showDetailsData();
+        } else {
+            Intent intent = new Intent(this, DetailActivity.class);
+            intent.putExtra(DetailActivity.TO_SHOW, DetailActivity.DETAILS);
+            startActivity(intent);
+        }
     }
 
     private DetailFragment getDetailFragment() {
-        FragmentManager fm = getFragmentManager();
-        return (DetailFragment) fm.findFragmentById(R.id.detailFragment);
+        return (DetailFragment) getFragmentManager().findFragmentById(R.id.detailFragment);
     }
 }
